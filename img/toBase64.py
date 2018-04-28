c125476a67cf916b7f781c498d2087a50bf03c12
csy <chengshiyu0605@163.com>
2018-04-28 17:10:50 +0800

    Site updated: 2018-04-28 17:10:48

--- img/toBase64.py
+++ img/toBase64.py
@@ -1,6 +1,6 @@
 
 import base64
-f = open(r'C:\Users\Administrator\Desktop\0426\2.png', 'rb')
+f = open(r'C:\Users\Administrator\Desktop\0428\3.png', 'rb')
 ls_f = base64.b64encode(f.read())
 f.close()
 print(ls_f)
