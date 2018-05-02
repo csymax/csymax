8a3e02a9aee01734fd607a3cfa17e9f721656cba
csy <chengshiyu0605@163.com>
2018-05-02 18:07:15 +0800

    Site updated: 2018-05-02 18:07:11

--- img/toBase64.py
+++ img/toBase64.py
@@ -1,6 +1,9 @@
 
 import base64
 f = open(r'C:\Users\Administrator\Desktop\0428\3.png', 'rb')
+txtf = open(r'C:\Users\Administrator\Desktop\0428\1.txt', 'w')
 ls_f = base64.b64encode(f.read())
 f.close()
-print(ls_f)
+# print(ls_f)
+txtf.write(str(ls_f))
+txtf.close()
